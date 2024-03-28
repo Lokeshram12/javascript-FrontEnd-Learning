21fb7db3a6800509844050c5921181fd83515587
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-03-28 20:14:11 +0000

    datatypes,variables

---
+++ 1_basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hi lokesh") we ar using nodejs,not browser
+
+console.log(3*3); //code readability
+
+// number
+//bigint
+// string=>""
+// boolean =>true/fals
+// null =>standalone value
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ 1_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId=45
+let accountEmail="l@gmail.com"
+var accountPassword="264"
+accountCity="abc"
+
+// accountId=2 //const data type cannot be changed once intitalized
+accountEmail="B@bc.com"
+accountPassword=55465
+accountCity="beng"
+
+
+/*
+prefer not to use var as there can be block and function scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
