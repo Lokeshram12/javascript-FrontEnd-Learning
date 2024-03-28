826125e4a803984e7a5c22d267bfecda05dcee2b
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-03-28 20:46:41 +0000

    comparision operators,dataTypes summary

--- 1_basics/conversionOperations.js
+++ 1_basics/conversionOperations.js
@@ -9,3 +9,21 @@ console.log(newAge); //NaN
 // "33" => 33
 // "33abc" => NaN
 // true=>1
+
+
+// *****operation *******
+
+let value=3
+let negValue=-value
+
+console.log(negValue);
+
+console.log("1" + 2 + 2); //122
+
+console.log("1" + "2" + 2); //122
+
+console.log(1 + 2 + "2"); //32
+
+
+//operator precedence
+
---
+++ datatypes-summaries.js
@@ -0,0 +1,32 @@
+// primitive -call by value
+
+// 7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+//Dynamic types 
+
+// non primitive/reference type
+
+// Array,Objects,Functions
+
+
+const id=Symbol('12')
+
+const anotherId=Symbol('12')
+
+console.log(id === anotherId); //false
+
+const heros=["a","b","c"]
+
+let myObj={
+    name:"ram",
+    age:22
+}
+
+
+const myFunction=function(){
+console.log("hii");
+}
+
+()
+console.log(myFunction);
