15431914521d5df565f8ce610b14fbd600635454
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-04-07 23:07:43 +0000

    added string concepts

---
+++ 1_basics/strings.js
@@ -0,0 +1,31 @@
+const name="lokesh"
+
+console.log(name + " ram");
+
+console.log(`My name is ${name}`);
+
+const gameName= new String('lokesh') //string is a object
+
+console.log(gameName);
+
+console.log(gameName[2]);
+
+console.log(gameName.length);
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('e'));
+
+const newString=gameName.substring(0,4)
+
+console.log(newString);
+
+const  anotherString=gameName.slice(-8,4)
+
+// trim()
+
+//replcae( , )
+
+//includes()
+
+//split() -gives 
